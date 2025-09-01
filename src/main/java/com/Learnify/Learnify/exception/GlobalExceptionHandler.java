1985421b14ebee84c63648755dd3de94f5366aa3
vittorhonorato <vittorhonoratoo@gmail.com>
2025-09-01 08:19:47 -0300

    Novo handler para lançar mensagem de erro personalizada para o @Validation

--- src/main/java/com/Learnify/Learnify/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Learnify/Learnify/exception/GlobalExceptionHandler.java
@@ -2,14 +2,15 @@ package com.Learnify.Learnify.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
     private ResponseEntity<ErrorResponse> taskNotFoundException(TaskNotFoundException ex) {
@@ -20,6 +21,22 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(InvalidTaskException.class)
     private ResponseEntity<ErrorResponse> invalidTaskException(InvalidTaskException ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, 404, ex.getMessage(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST,
+                400,
+                errorMessage,
+                LocalDateTime.now()
+        );
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
